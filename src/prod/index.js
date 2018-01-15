1e0245463bdfdd64b8b2c9b4eb2477c2104ed568
Mike Ng <mike.esokia@gmail.com>
2018-01-15 17:37:56 +0400

    remove bower.json

--- src/prod/index.js
+++ src/prod/index.js
@@ -2,12 +2,12 @@ import $ from 'jquery';
 require('./style/main.scss');
 import * as utils from './../components/utils/utils.js';
 import bootstrap from './bootstrap.js';
-require('./../../bower_components/jquery-ui/ui/i18n/datepicker-ar.js');
-require('./../../bower_components/jquery-ui/ui/i18n/datepicker-de.js');
-require('./../../bower_components/jquery-ui/ui/i18n/datepicker-es.js');
-require('./../../bower_components/jquery-ui/ui/i18n/datepicker-fr.js');
-require('./../../bower_components/jquery-ui/ui/i18n/datepicker-nl.js');
-require('./../../bower_components/jquery-ui/ui/i18n/datepicker-en-GB.js');
+require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-ar.js');
+require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-de.js');
+require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-es.js');
+require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-fr.js');
+require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-nl.js');
+require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-en-GB.js');
 
 $.widget.bridge('uitooltip', $.ui.tooltip);
 //window.btn = $.fn.button.noConflict(); // reverts $.fn.button to jqueryui btn
