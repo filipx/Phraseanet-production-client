67e143ffe89bd9c25acd724bb6b8ae13c9b0e2ff
Mike Ng <mike.esokia@gmail.com>
2018-03-27 13:04:01 +0400

    remove height calculation since we are using pym.js

--- src/components/record/recordPreview/index.js
+++ src/components/record/recordPreview/index.js
@@ -307,10 +307,10 @@ const previewRecordService = services => {
                         ''
                     );
                     //set height of iframe to 100%
-                    activeThumbnailFrame.iframe.setAttribute(
-                        'height',
-                        '100%'
-                    );
+                    // activeThumbnailFrame.iframe.setAttribute(
+                    //     'height',
+                    //     '100%'
+                    // );
                     /*
                      // warning - if listening events/sendings events,
                      // pym instances should be destroyed when preview is closed
